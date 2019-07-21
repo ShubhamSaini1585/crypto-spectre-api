ba032d7549b4146415d325eb2704a3f3a760ac5d
Kunal Deshpande <kunaldeshpande9896@gmail.com>
2019-07-21 18:05:50 +0530

    Remove unecessary files

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 // Added new route
 var resultRouter = require("./routes/result");
 
@@ -21,9 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // Using new router
 app.use("/result", resultRouter);
